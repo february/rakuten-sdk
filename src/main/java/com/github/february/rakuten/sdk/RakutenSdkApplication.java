fbebfa0f506a16c7bd0958eaba69d8e10e9c03a6
february <zhang.hao@live.com>
2018-12-07 22:08:02 +0800

    初始化构建

---
+++ src/main/java/com/github/february/rakuten/sdk/RakutenSdkApplication.java
@@ -0,0 +1,12 @@
+package com.github.february.rakuten.sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakutenSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakutenSdkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/february/rakuten/sdk/RakutenSdkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.february.rakuten.sdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RakutenSdkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
